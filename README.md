789482356a8ba72a1f0906c183ab2a9ab40531b2
Sridhar Ratnakumar <srid@srid.ca>
2023-10-04 17:06:00 -0400

    Use dioxus package; do hello world

---
+++ README.md
@@ -0,0 +1,11 @@
+# dioxus-desktop-template
+
+WIP: A starter template for Dioxus Desktop apps w/ Tailwind & Nix
+
+## Tasks
+
+- [x] Hello World Dioxus project
+- [ ] Tailwind
+- [ ] macOS bundling
+- [ ] Routes
+- [ ] Application state
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+#![allow(non_snake_case)]
+// import the prelude to get access to the `rsx!` macro and the `Scope` and `Element` types
+use dioxus::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    // launch the dioxus app in a webview
+    dioxus_desktop::launch(App);
+}
+
+// define a component that renders a div with the text "Hello, world!"
+fn App(cx: Scope) -> Element {
+    cx.render(rsx! {
+        div {
+            "Hello, world!"
+        }
+    })
 }
