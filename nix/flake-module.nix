570500423c5b5d4734d120d920af5dbd06a1dc79
Sridhar Ratnakumar <srid@srid.ca>
2023-10-20 11:27:49 -0400

    Add Jenkinsfile

--- nix/flake-module.nix
+++ nix/flake-module.nix
@@ -20,6 +20,8 @@ in
             default = [ ] ++ lib.optionals pkgs.stdenv.isLinux (with pkgs; [
               webkitgtk_4_1
             ]) ++ lib.optionals pkgs.stdenv.isDarwin (
+              # Use newer SDK because some crates require it
+              # cf. https://github.com/NixOS/nixpkgs/pull/261683#issuecomment-1772935802
               with pkgs.darwin.apple_sdk.frameworks; [
                 IOKit
                 Carbon
