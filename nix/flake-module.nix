9381eb302bcb0d94f856fddaea95c633ccd1e77e
Sridhar Ratnakumar <srid@srid.ca>
2024-03-24 09:47:22 -0400

    linux: add xdo

--- nix/flake-module.nix
+++ nix/flake-module.nix
@@ -19,6 +19,7 @@ in
             type = lib.types.listOf lib.types.package;
             default = [ ] ++ lib.optionals pkgs.stdenv.isLinux (with pkgs; [
               webkitgtk_4_1
+              xdo
             ]) ++ lib.optionals pkgs.stdenv.isDarwin (
               with pkgs.darwin.apple_sdk.frameworks; [
                 IOKit
