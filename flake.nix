b3f00a16ef6deb02fb2068cfcb57fed1df7816c8
Sridhar Ratnakumar <srid@srid.ca>
2024-04-19 16:36:09 +1000

    feat(nix): Switch to rust-flake

--- flake.nix
+++ flake.nix
@@ -10,10 +10,8 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
     systems.url = "github:nix-systems/default";
 
-    rust-overlay.url = "github:oxalica/rust-overlay";
-    # https://github.com/ipetkov/crane/issues/527
-    crane.url = "github:ipetkov/crane/2c653e4478476a52c6aa3ac0495e4dea7449ea0e"; # Cargo.toml parsing is broken in newer crane (Mar 24)
-    crane.inputs.nixpkgs.follows = "nixpkgs";
+    rust-flake.url = "github:juspay/rust-flake";
+    rust-flake.inputs.nixpkgs.follows = "nixpkgs";
     treefmt-nix.url = "github:numtide/treefmt-nix";
     treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
     process-compose-flake.url = "github:Platonic-Systems/process-compose-flake";
@@ -25,10 +23,11 @@
       systems = import inputs.systems;
 
       imports = [
+        inputs.rust-flake.flakeModules.default
+        inputs.rust-flake.flakeModules.nixpkgs
         inputs.treefmt-nix.flakeModule
         inputs.process-compose-flake.flakeModule
         inputs.cargo-doc-live.flakeModule
-        ./nix/flake-module.nix
       ];
 
       flake = {
@@ -39,13 +38,6 @@
       };
 
       perSystem = { config, self', pkgs, lib, system, ... }: {
-        _module.args.pkgs = import inputs.nixpkgs {
-          inherit system;
-          overlays = [
-            inputs.rust-overlay.overlays.default
-          ];
-        };
-
         # Add your auto-formatters here.
         # cf. https://numtide.github.io/treefmt/
         treefmt.config = {
@@ -56,7 +48,75 @@
           };
         };
 
-        packages.default = self'.packages.dioxus-desktop-template;
+        nixpkgs.overlays = [
+          # Configure tailwind to enable all relevant plugins
+          (self: super: {
+            tailwindcss = super.tailwindcss.overrideAttrs
+              (oa: {
+                plugins = [
+                  pkgs.nodePackages."@tailwindcss/aspect-ratio"
+                  pkgs.nodePackages."@tailwindcss/forms"
+                  pkgs.nodePackages."@tailwindcss/language-server"
+                  pkgs.nodePackages."@tailwindcss/line-clamp"
+                  pkgs.nodePackages."@tailwindcss/typography"
+                ];
+              });
+          })
+        ];
+
+        rust-project = {
+          crane.args = {
+            buildInputs = lib.optionals pkgs.stdenv.isLinux
+              (with pkgs; [
+                webkitgtk_4_1
+                xdotool
+              ]) ++ lib.optionals pkgs.stdenv.isDarwin (
+              with pkgs.darwin.apple_sdk.frameworks; [
+                IOKit
+                Carbon
+                WebKit
+                Security
+                Cocoa
+              ]
+            );
+            nativeBuildInputs = with pkgs;[
+              pkg-config
+              makeWrapper
+              tailwindcss
+              dioxus-cli
+            ];
+          };
+          src = lib.cleanSourceWith {
+            src = inputs.self; # The original, unfiltered source
+            filter = path: type:
+              (lib.hasSuffix "\.html" path) ||
+              (lib.hasSuffix "tailwind.config.js" path) ||
+              # Example of a folder for images, icons, etc
+              (lib.hasInfix "/assets/" path) ||
+              (lib.hasInfix "/css/" path) ||
+              # Default filter from crane (allow .rs files)
+              (config.rust-project.crane.lib.filterCargoSources path type)
+            ;
+          };
+        };
+
+        packages.default = self'.packages.dioxus-desktop-template.overrideAttrs (oa: {
+          # Copy over assets for the desktop app to access
+          installPhase =
+            (oa.installPhase or "") + ''
+              cp -r ./assets/* $out/bin/
+            '';
+          postFixup =
+            (oa.postFixup or "") + ''
+              # HACK: The Linux desktop app is unable to locate the assets
+              # directory, but it does look into the current directory.
+              # So, `cd` to the directory containing assets (which is
+              # `bin/`, per the installPhase above) before launching the
+              # app.
+              wrapProgram $out/bin/${config.rust-project.cargoToml.package.name} \
+                --chdir $out/bin
+            '';
+        });
 
         devShells.default = pkgs.mkShell {
           name = "dioxus-desktop-template";
@@ -66,9 +126,6 @@
           ];
           packages = with pkgs; [
             just
-            # Dioxus
-            dioxus-cli
-
           ];
           shellHook = ''
             echo
