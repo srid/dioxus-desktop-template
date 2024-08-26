d6ce3f843c899cb3a69cfa0485eeb4aecee6cf4e
Sridhar Ratnakumar <srid@srid.ca>
2024-08-26 16:31:58 -0400

    Remove garnix

--- flake.nix
+++ flake.nix
@@ -1,10 +1,5 @@
 {
   description = "A starter template for Dioxus Desktop apps w/ Tailwind & Nix";
-  nixConfig = {
-    # https://garnix.io/docs/caching
-    extra-substituters = "https://cache.garnix.io";
-    extra-trusted-public-keys = "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g=";
-  };
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
